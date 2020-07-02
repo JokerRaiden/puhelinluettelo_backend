885103df3b4e05256df385c4c9a7e7aefb81cca5
JokerRaiden <joona.rahkonen@gmail.com>
2020-07-02 20:31:06 +0300

    uibuild

---
+++ build/precache-manifest.b55bb2e3dcd5c82805379449b40fb13e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce287b9ca8bb1d990e0034f85b0e6ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c32c2e475c3fd9dd0c17",
+    "url": "/static/css/main.cdc672df.chunk.css"
+  },
+  {
+    "revision": "77ed0c26fb7a102b89fb",
+    "url": "/static/js/2.936a527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.936a527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32c2e475c3fd9dd0c17",
+    "url": "/static/js/main.974f2901.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
